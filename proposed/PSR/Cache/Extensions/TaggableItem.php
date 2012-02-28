78d672a5c977d7aabc7b335445a6ca0028cebaaa
Robert Hafner <tedivm@tedivm.com>
2012-02-28 00:55:49 -0800

    Added the Stackable and TaggableItem files.

---
+++ proposed/PSR/Cache/Extensions/TaggableItem.php
@@ -0,0 +1,23 @@
+namespace PSR\Cache\Extensions;
+
+/**
+ * Cache\Extensions\TaggableItem extends Cache\Item to provide tagging support.
+ *
+ * The Cache\Extensions\TaggableItem interface adds support for tagging to the
+ * base Cache\Item interface. Items can be added to multiple categories (called
+ * tags) that can be used for group invalidation.
+ */
+interface TaggableItem extends \PSR\Cache\Item
+{
+    /**
+     * Sets the tags for the current item.
+     *
+     * Accepts an array of strings for the item to be tagged with. The tags
+     * passed should overwrite any existing tags, and passing an empty array
+     * will cause all tags to be removed. Changes to an Item's tags are not
+     * guaranteed to persist unless the "set" function is called.
+     *
+     * @return void
+     */
+    function setTags(array $tags = array());
+}
