21c06c6e4b0ecdc00bf76326fdcdc18c1ea5cf40
Paul M. Jones <pmjones88@gmail.com>
2013-07-29 22:38:46 -0500

    move implementations to test file

--- proposed/autoloader/AutoloadTest.php
+++ proposed/autoloader/AutoloadTest.php
@@ -2,14 +2,21 @@
 namespace Example;
 
 /**
- * An implementation of a project-specific autoload function.
+ * An example of of a project-specific implementation.
  * 
+ * After registering this autoload function with SPL, the following line
+ * would cause the function to attempt to load the \Foo\Bar\Baz\Qux class
+ * from /path/to/project/src/Baz/Qux.php:
+ * 
+ *      new \Foo\Bar\Baz\Qux;
+ *      
  * @param string $class The fully-qualified class name.
  * @return void
  */
 function autoloadFunction($class)
 {
-    // PSR-T: normalize the project namespace prefix
+    // PSR-T: normalize the project namespace prefix; note the leading and
+    // trailing separators.
     $prefix = '\\Foo\\Bar\\';
     
     // PSR-T: normalize a leading backslash if not already present
@@ -32,14 +39,54 @@ function autoloadFunction($class)
     
     // PSR-X: append .php and find the file
     $file .= '.php';
-    if (is_readable($file)) {
-        include $file;
+    if (file_exists($file)) {
+        require $file;
     }
 }
 
 /**
- * An implementation that includes the optional functionality of allowing
- * multiple base directories for a single namespace prefix.
+ * An example of a general-purpose implementation that includes the optional
+ * functionality of allowing multiple base directories for a single namespace
+ * prefix.
+ * 
+ * Given a `foo-bar` package of classes in the file system at the following
+ * paths ...
+ * 
+ *     /path/to/packages/foo-bar/
+ *         src/
+ *             Baz.php             # Foo\Bar\Baz
+ *             Qux/
+ *                 Quux.php        # Foo\Bar\Qux\Quux
+ *         tests/
+ *             BazTest.php         # Foo\Bar\BazTest
+ *             Qux/
+ *                 QuuxTest.php    # Foo\Bar\Qux\QuuxTest
+ * 
+ * ... add the path to the class files for the `\Foo\Bar\` namespace prefix
+ * as follows:
+ * 
+ *      <?php
+ *      // instantiate the loader
+ *      $loader = new \Example\AutoloadClass;
+ *      
+ *      // register the autoloader
+ *      $loader->register();
+ *      
+ *      // register the base directories for the namespace prefix
+ *      $loader->addNamespace('Foo\Bar', '/path/to/packages/foo-bar/src');
+ *      $loader->addNamespace('Foo\Bar', '/path/to/packages/foo-bar/tests');
+ * 
+ * The following line would cause the autoloader to attempt to load the
+ * \Foo\Bar\Baz\Qux class from /path/to/packages/foo-bar/src/Qux/Quux.php:
+ * 
+ *      <?php
+ *      new \Foo\Bar\Baz\Qux;
+ * 
+ * The following line would cause the autoloader to attempt to load the 
+ * \Foo\Bar\Baz\Qux\QuuxTest class from /path/to/packages/foo-bar/tests/Qux/QuuxTest.php:
+ * 
+ *      <?php
+ *      new \Foo\Bar\Baz\Qux\QuuxTest;
  */
 class AutoloadClass
 {
